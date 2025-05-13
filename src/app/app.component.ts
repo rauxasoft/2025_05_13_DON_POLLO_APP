769622fd432cf10ef7a2f5332edd1f2702f44d79
jordi from virtual machine <jordi@gmail.com>
2025-05-13 19:07:07 +0200

    ahora se puede hacer logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, effect, inject, OnInit } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { APP_VERSION } from './tokens/app-version.token';
 import { userPayload } from './core/stores/user.store';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,33 +11,23 @@ import { userPayload } from './core/stores/user.store';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
 
   appVersion = inject(APP_VERSION);
-  nombre: string | null = null;     // no utilizamos signal() porque los datos no van a cambiar. Ya está bien.
-  roles: string[] | null = null     // no utilizamos signal() porque los datos no van a cambiar. Ya está bien.
+  private auth = inject(AuthService);
 
-  constructor(){
-    
+  nombre: string | null = null;
+  roles: string[] | null = null;
+
+  constructor() {
     effect(() => {
       const payload = userPayload();
-      this.nombre = payload?.nombre ?? 'XXXXXXXSDSDSDS';
-      this.roles = payload?.roles ?? ['mindundi'];
+      this.nombre = payload?.nombre ?? null;
+      this.roles = payload?.roles ?? null;
     });
-    
   }
 
-  ngOnInit() {
-    const raw = localStorage.getItem('user_payload');
-    if(raw) {
-      try{
-        const payload = JSON.parse(raw);
-        this.nombre = payload?.nombre || null;
-        this.roles = payload?.roles || null;
-      } catch (error) {
-        console.error('Error al obtener los datos del usuario a partir del token:', error);
-      }
-    }
+  logout() {
+    this.auth.logout();
   }
-
 }
