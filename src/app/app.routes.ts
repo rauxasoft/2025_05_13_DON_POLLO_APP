28af2cbcc9d27ece5967a086ab86c9405d645c47
jordi from virtual machine <jordi@gmail.com>
2025-05-14 16:50:51 +0200

    comletada la fase 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,19 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ListadoFamiliasComponent } from './components/listado-familias/listado-familias.component';
 import { LoginComponent } from './components/login/login.component';
-import { ListadoEstablecimientosComponent } from './components/listado-establecimientos/listado-establecimientos.component';
+//import { ListadoEstablecimientosComponent } from './components/listado-establecimientos/listado-establecimientos.component';
+import { roleGuard } from './core/guards/role.guard';
 
 export const routes: Routes = [
 
     {path: '', component: HomeComponent},
     {path: 'familias', component: ListadoFamiliasComponent},
-    {path: 'establecimientos', component: ListadoEstablecimientosComponent},
+//    {path: 'establecimientos', component: ListadoEstablecimientosComponent},
     {path: 'login', component: LoginComponent},
+    {
+        path: 'establecimientos',
+        canActivate: [roleGuard('ADMIN')],
+        loadComponent: () => import('./components/listado-establecimientos/listado-establecimientos.component').then(m => m.ListadoEstablecimientosComponent)
+    },
     {path: '**', redirectTo: ''}
 ];
