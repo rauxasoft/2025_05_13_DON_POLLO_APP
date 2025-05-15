8b343e112d668179b036c114e618ba6144329171
jordi from virtual machine <jordi@gmail.com>
2025-05-15 09:58:24 +0200

    se cambia el nombre del proyecto dentro de angular.json y se instala i18n

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+/// <reference types="@angular/localize" />
+
+// la triple barra es una directiva de TypeScript para incluir definiciones de tipos. Util para autocompletado.
+// pero no carga el código necesario para que $localize funcione.
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
